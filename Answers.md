d8bce3b99313fc155577ee0a18dbdfcbe6e90eab
Achal <achaldev3@gmail.com>
2024-03-11 14:13:32 +0530

    upload answer

---
+++ Answers.md
@@ -0,0 +1,5 @@
+1. The relationship between the product and product_category entities is established by a foreign key column in the product table that
+refers to the primary key in the product_category table. This foreign key is used to associate each product with a specific category.
+
+2. For this we can use a foreign key constraint. This constraint ensures that the foreign key values in the product table corresponds to
+existing primary key value in the referenced table.
---
+++ schema.sql
@@ -0,0 +1,48 @@
+CREATE SCHEMA product_schema;
+
+USE product_schema;
+
+CREATE TABLE product_category(
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    desc TEXT,
+    created_at TIMESTAMP CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE product_inventory(
+    id INT PRIMARY KEY,
+    quantity INT,
+    created_at TIMESTAMP CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE discount(
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    desc TEXT,
+    discount_percent DECIMAL(5, 2),
+    active BOOLEAN,
+    created_at TIMESTAMP CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE product(
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    desc TEXT,
+    SKU VARCHAR(255),
+    category_id INT,
+    inventory_id INT,
+    price decimal(10, 2),
+    discount_id INT,
+    created_at TIMESTAMP CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES product_category(id),
+    FOREIGN KEY (inventory_id) REFERENCES product_inventory(id),
+    FOREIGN KEY (discount_id) REFERENCES discount(id)
+);
